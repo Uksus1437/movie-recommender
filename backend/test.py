18389cede2d122b217e63965b1717e546551c320
Uksus1437 <116167254+Uksus1437@users.noreply.github.com>
2025-02-19 00:40:45 +0300

    Добавил автозаволнение фильмов в строке поиска

--- backend/test.py
+++ backend/test.py
@@ -1,9 +1,33 @@
 import requests
+import pandas as pd
 
-url = "http://127.0.0.1:5000/recommend"
-params = {"movie_id": 11, "top_n": 10}
+url_recommend = "http://127.0.0.1:5000/recommend"
+processed_data = pd.read_csv("data/final_process_data.csv")
 
-response = requests.get(url, params=params)
-data = response.json()
+# movie = '«Чудотворец» из Бирюлёва'
 
-print("Рекомендации:", data["recommended_movies"])
+# movie_id = processed_data[processed_data['title'] == movie].index[0]
+
+# params = {"movie_id": movie_id, "top_n": 5}
+
+# response = requests.get(url_recommend, params=params)
+# data = response.json()
+
+# print("Рекомендации:", data["recommended_movies"])
+
+
+url_search = "http://127.0.0.1:5000/search"
+
+query = 'из'
+params = {"query": query}
+
+response = requests.get(url_search, params=params)
+
+
+if response.status_code == 200:
+    data = response.json()
+    print("Найденные фильмы:")
+    for movie in data:
+        print(f"{movie['index']}: {movie['title']}")
+else:
+    print("Ошибка:", response.status_code)
